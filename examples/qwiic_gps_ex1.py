722c0d95cd4f59a3228dc8494caaaca78a915d33
edspark <elias.santistevan@sparkfun.com>
2019-11-22 16:13:30 -0700

    Adds example code that prints out latitude, longitude, and time

--- examples/qwiic_gps_ex1.py
+++ examples/qwiic_gps_ex1.py
@@ -1,4 +1,70 @@
-import time
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# qwiic_gps_ex1.py
+#
+# Simple Example for SparkFun GPS Breakout - XA1110
+#------------------------------------------------------------------------
+#
+# Written by  SparkFun Electronics, October 2019
+#
+#
+# More information on qwiic is at https://www.sparkfun.com/qwiic
+#
+# Do you like this library? Help support SparkFun. Buy a board!
+#
+#==================================================================================
+# Copyright (c) 2019 SparkFun Electronics
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#==================================================================================
+# Example 1
+#
+
+from time import sleep
 import sys
 import qwiic_i2c_driver
-# Place holder
+
+def run_example():
+
+    print("SparkFun u-blox GPS!")
+    qwiicGPS = qwiic_titan_gps.QwiicGps()
+
+    if qwiicGPS.connected is False:
+        print("Could not connect to to the SparkFun GPS Unit. Double check that\
+              it's wired correctly.", file=sys.stderr)
+        return
+
+    qwiicGPS.begin()
+
+    while True:
+        if get_nmea_data() is True:
+            print("Latitude: {}, Longitude: {}, Time: {}".format(
+                    gnss_messages['Latitude'],
+                    gnss_messages['Longitude'],
+                    gnss_messages['Time']))
+
+        sleep(1)
+
+
+if __name__ == '__main__':
+    try:
+        run_example()
+    except (KeyboardInterrupt, SystemExit) as exErr:
+        print("Ending Basic Example.")
+        sys.exit(0)
