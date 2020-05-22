9ef668c63850c4d51e9fd7eac965ba75271613f5
Ho Yun "Bobby <ho.chan@colorado.edu>
2020-05-22 00:31:33 -0600

    Update conf.py
    Broken links in readthedocs so updating version number.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, SparkFun Electronics'
 author = 'SparkFun Electronics'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
